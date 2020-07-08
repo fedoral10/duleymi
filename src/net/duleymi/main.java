61519c20aab690f4911288b303b44feacc10928d
Jorge <fedoral10@gmail.com>
2020-07-08 13:52:37 -0600

    Agregado funcion de creacion de procesos aleatorios

--- src/net/duleymi/main.java
+++ src/net/duleymi/main.java
@@ -16,8 +16,6 @@ import net.duleymi.procesos.DefaultProceso;
  */
 public class main {
 
-    
-    
     /**
      * @param args the command line arguments
      */
@@ -27,27 +25,69 @@ public class main {
     }
 
     public static List<DefaultProceso> getMemoria() {
-        List<DefaultProceso> memoria = new ArrayList<>();
+        /**
+         * ******************* Creacion de Objetos Procesos ************************
+         */
+        /*List<DefaultProceso> memoria = new ArrayList<>();
         DefaultProceso p1 = new DefaultProceso();
         p1.setNombre("Proceso 1");
         p1.setPrioridad(DefaultProceso.Prioridad.BAJA);
         p1.setTiempoEjecucion(90);
-        
+
         DefaultProceso p2 = new DefaultProceso();
         p2.setNombre("Proceso 2");
         p2.setPrioridad(DefaultProceso.Prioridad.ALTA);
         p2.setTiempoEjecucion(100);
-        
+
         DefaultProceso p3 = new DefaultProceso();
         p3.setNombre("Proceso 3");
         p3.setPrioridad(DefaultProceso.Prioridad.MEDIA);
-        p3.setTiempoEjecucion(10);
-        
-        memoria.add(p1);
+        p3.setTiempoEjecucion(10);*/
+
+        /**
+         * *************** Agregarlos a memoria ******************
+         */
+        /*memoria.add(p1);
         memoria.add(p2);
         memoria.add(p3);
+
+        return memoria;*/
+        return generarProcesosAleatorios(10);
+    }
+
+    private  static DefaultProceso.Prioridad prioridaRandom(){
+        int numeroAleatorio = (int) (1 + Math.random() * (3 - 1));
+        
+        switch(numeroAleatorio){
+            case 1:
+                return DefaultProceso.Prioridad.BAJA;
+                
+            case 2:
+                return DefaultProceso.Prioridad.MEDIA;
+                
+            case 3:
+                return DefaultProceso.Prioridad.MEDIA;
+        }
+        System.out.println("XXXX");
+        return DefaultProceso.Prioridad.BAJA;
+    }
+    
+    private static List<DefaultProceso> generarProcesosAleatorios(int cantidad) {
+        List<DefaultProceso> memoria = new ArrayList<>();
+        for (int i = 0; i < cantidad; i++) {
+            
+            DefaultProceso p = new DefaultProceso();
+            
+            p.setNombre("Proceso "+ (i+1));
+            p.setPrioridad(prioridaRandom());
+            
+            p.setTiempoEjecucion((int) (1 + Math.random() * (100 - 1)) );
+            
+            memoria.add(p);
+            
+        }
         
         return memoria;
     }
-    
+
 }
