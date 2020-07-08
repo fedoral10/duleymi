9c0a4e3fdadc3d7c99bbb387222b945232179773
Jorge <fedoral10@gmail.com>
2020-07-08 11:30:27 -0600

    primer commit

---
+++ src/net/duleymi/algoritmos/IAlgoritmo.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package net.duleymi.algoritmos;
+
+/**
+ *
+ * @author USUARIO
+ */
+public interface IAlgoritmo {
+        
+    public void ejecutarProceso();
+
+}
